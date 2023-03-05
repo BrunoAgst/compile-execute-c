836e6b718f10f7faa90c4020e86bad4c547ec0cb
Bruno Augusto <bruno_agst@hotmail.com>
2023-03-05 01:55:13 -0300

    v1

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main(){
+  var fileName = os.Args[1]
+  
+  //compile file
+  compile := exec.Command("/usr/bin/gcc", fileName, "-o", "output")
+  _, err := compile.Output()
+  if err != nil {
+    fmt.Println(err.Error())
+    return
+  }
+
+  //run file
+  run := exec.Command("./output")
+  stout, err2 := run.Output()
+  if err2 != nil {
+    fmt.Println(err2.Error())
+    return
+  }
+  fmt.Println(string(stout))
+  
+  //delete file
+  err3 := os.Remove("output")
+  if err3 != nil {
+    fmt.Println(err3.Error())
+    return
+  }
+}    
+
