58c4f918b721f27cd36db69dac369f6500ecf525
Bruno Augusto <bruno_agst@hotmail.com>
2023-03-11 16:02:57 -0300

    filter the list of output boards

--- upload-arduino/main.go
+++ upload-arduino/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"regexp"
+)
+
+func main(){
+  cmd, err := exec.Command("arduino-cli", "board", "list").Output()
+  if err != nil{
+    log.Fatal("err", err.Error())
+    return
+  }
+
+  match := regexp.MustCompile("a([a-z]+):([a-z]+):([a-z]+)")
+  fqbn := match.FindString(string(cmd))
+  fmt.Println(fqbn)
+}
+
