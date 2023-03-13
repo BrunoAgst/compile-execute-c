4ffac4b426f3ad7c214dd908245ecc906e4c59b1
Bruno Augusto <bruno_agst@hotmail.com>
2023-03-13 14:53:40 -0300

    finish upload arduino v1.0

--- upload-arduino/main.go
+++ upload-arduino/main.go
@@ -1,21 +1,56 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"regexp"
 )
 
 func main(){
+
+  var projectName = os.Args[1]
+
   cmd, err := exec.Command("arduino-cli", "board", "list").Output()
+  
   if err != nil{
-    log.Fatal("err", err.Error())
+    log.Fatal("error listBoard: ", err.Error())
     return
   }
 
-  match := regexp.MustCompile("a([a-z]+):([a-z]+):([a-z]+)")
-  fqbn := match.FindString(string(cmd))
-  fmt.Println(fqbn)
+  fqbn := getFqbn(cmd)
+  
+  compile(fqbn, projectName)
+
+  upload(fqbn, cmd, projectName)
+  
+  log.Println("completed sucessfully")
 }
 
+func getFqbn(command []byte) string {
+  match := regexp.MustCompile("a([a-z]+):([a-z]+):([a-z]+)")
+  fqbn := match.FindString(string(command))
+  
+  return fqbn
+}
+
+func compile(fqbn string, name string) {
+  cmd, err := exec.Command("arduino-cli", "compile", "--fqbn", fqbn, name).Output()
+
+  if err != nil {
+    log.Fatal("error Compile: ", err.Error())
+  }
+
+  log.Println(string(cmd))
+}
+
+func upload(fqbn string, cmd []byte, name string) {
+  match := regexp.MustCompile("/dev/cu.([a-z0-9]+)")
+  pathBoard := match.FindString(string(cmd))
+
+  _, err := exec.Command("arduino-cli", "upload", "-p", pathBoard, "--fqbn", fqbn, name).Output()
+
+  if err != nil {
+    log.Fatal("err upload: ", err.Error())
+  }
+}
